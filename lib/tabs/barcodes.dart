f7ff60ae6ca2fbe055a12c2d7bc005f56e109b21
Adam O'Brien <17226864@studentmail.ul.ie>
2021-05-19 14:10:55 +0100

    Fixed database implementation for barcodes
    Restructured barcodes tab

--- lib/tabs/barcodes.dart
+++ lib/tabs/barcodes.dart
@@ -20,7 +20,7 @@ class _BarcodesState extends State<Barcodes> {
   }
 
   Future _loadBarcodes() async {
-    DatabaseHelper.instance.loadAll().then((barcodes) {
+    DatabaseService.instance.loadAll().then((barcodes) {
       setState(() {
         // Empty _barcodesList
         _barcodesList.clear();
@@ -33,7 +33,7 @@ class _BarcodesState extends State<Barcodes> {
   }
 
   _delete(int index) async {
-    DatabaseHelper helper = DatabaseHelper.instance;
+    DatabaseService helper = DatabaseService.instance;
     bool deleted = await helper.deleteBarcodeByIndex(index);
     if (deleted) {
       print('barcode deleted');
@@ -71,8 +71,9 @@ class _BarcodesState extends State<Barcodes> {
 
     print('Result: ' + barcodeScanRes);
 
-    if (barcodeScanRes == null || barcodeScanRes == '-1') {
-      return false;
+    if (barcodeScanRes == '-1') {
+      // User has closed barcode scanner
+      barcodeScanRes = "example";
     }
 
     int iRows = await BarcodeService.addBarcode(barcodeScanRes);
@@ -82,34 +83,73 @@ class _BarcodesState extends State<Barcodes> {
   @override
   Widget build(BuildContext context) {
     // TODO: Add AnimatedList to show barcode additions and removals
-    return Column(
-      children: <Widget>[
-        IconButton(
-            icon: Icon(Icons.camera_enhance_rounded),
-            onPressed: _scanNewBarcode),
-        RefreshIndicator(
-          child: ListView.builder(
-            padding: const EdgeInsets.all(8),
-            itemCount: _barcodesList.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                title: Text(_barcodesList[index].id.toString()),
-                subtitle: Text(_barcodesList[index].data),
-                trailing: IconButton(
-                  icon: Icon(Icons.delete_rounded),
-                  onPressed: () => {_delete(_barcodesList[index].id)},
+    return RefreshIndicator(
+      child: Column(
+        children: <Widget>[
+          _barcodesList.isEmpty
+              ? (TextButton(
+                  onPressed: _loadBarcodes,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(
+                          Icons.refresh_rounded,
+                          color: Colors.amber[600],
+                        ),
+                      ),
+                      Text(
+                        'Refresh Barcodes',
+                        style: TextStyle(color: Colors.amber[600]),
+                      )
+                    ],
+                  ),
+                ))
+              : ListView.builder(
+                  padding: const EdgeInsets.all(8),
+                  shrinkWrap: true,
+                  itemCount: _barcodesList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text(_barcodesList[index].id.toString()),
+                      subtitle: Text(_barcodesList[index].data),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete_rounded),
+                        onPressed: () => {_delete(_barcodesList[index].id)},
+                      ),
+                      onTap: () => showDialog(
+                        context: context,
+                        builder: (BuildContext context) =>
+                            _buildPopupDialog(context, index),
+                      ),
+                    );
+                  },
                 ),
-                onTap: () => showDialog(
-                  context: context,
-                  builder: (BuildContext context) =>
-                      _buildPopupDialog(context, index),
+          TextButton(
+            onPressed: _scanNewBarcode,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.camera_enhance_rounded,
+                    color: Colors.amber[600],
+                  ),
                 ),
-              );
-            },
+                Text(
+                  'Add new Barcode',
+                  style: TextStyle(color: Colors.amber[600]),
+                )
+              ],
+            ),
           ),
-          onRefresh: _loadBarcodes,
-        )
-      ],
+        ],
+      ),
+      onRefresh: _loadBarcodes,
     );
   }
 }
